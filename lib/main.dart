54d1af64bf7a57071d598355de9356f69c4999b3
JAbozarOid <araghibdoust@hotmail.com>
2021-05-19 17:30:01 +0430

    pink_part_designed

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [TopPart()],
+      body: Container(
+        child: Column(
+          children: [TopPart(), PinkPart()],
+        ),
       ),
     );
   }
@@ -118,6 +120,103 @@ class TopPart extends StatelessWidget {
                 ),
               ],
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PinkPart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180.0,
+      width: MediaQuery.of(context).size.width,
+      color: colorPurple,
+      child: Material(
+        color: colorPink,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(60.0),
+        ),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 30.0,
+            ),
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    'Today 5:30 PM',
+                    style: TextStyle(fontSize: 12.0, color: Colors.white70),
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    'Toga and Meditation for Begginers',
+                    style: TextStyle(
+                        fontSize: 22.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 50.0,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: 15.0,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                        width: 3.0, color: Colors.white)),
+                                child: CircleAvatar(
+                                  radius: 14.0,
+                                  backgroundImage:
+                                      ExactAssetImage('assets/sajad.jpg'),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                      width: 3.0, color: Colors.white)),
+                              child: CircleAvatar(
+                                radius: 14.0,
+                                backgroundImage:
+                                    ExactAssetImage('assets/farid.jpg'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'join Farid, Sajad & 10 others',
+                        style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.white70,
+                            fontStyle: FontStyle.italic),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
